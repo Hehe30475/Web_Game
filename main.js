078182d37737e5ed759f1896a5efef5040e67c1a
In1ecti0n <hadside696@gmail.com>
2024-09-13 22:55:21 +0700

    3

--- main.js
+++ main.js
@@ -4,6 +4,39 @@ let navbar= document.querySelector('.menu');
 menu.onclick = () => {
     navbar.classList.toggle('active');
     menu.classList.toggle('move');
+    bell.classList.remove('active');
+}
+
+let bell = document.querySelector('.notification');
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
 }
 
 
+var swiper = new Swiper(".trending-content", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay: 5000,
+        disableOnInteraction: false, 
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      720: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1068: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
+  
